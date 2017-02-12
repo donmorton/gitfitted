66692cdb08dc7fb3a433283ebf22f40d2e73aef4
Donald Morton <d3baseball2@gmail.com>
2017-02-12 09:14:59 -0500

    Updated Footers

--- index/script.js
+++ index/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$("button").click(function(){
+	$("button").not("#view-source").click(function(){
 		console.log($(this).attr("id"));
 		sort($(this).attr("id"));
 	});
