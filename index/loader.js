3dd68b93c2b18f54a798d7e1b9c57fbfe4891342
Donald Morton <d3baseball2@gmail.com>
2017-02-11 23:51:44 -0500

    Added card pusher

--- index/loader.js
+++ index/loader.js
@@ -4,6 +4,10 @@ $( document ).ready(function() {
 });
 
 function load(title, style, color){
-	$(".android-content mdl-layout__content").append("<p>" + title + "</p>");
-
+	var x = ("<div class = \"cardmain\"> <div class = 'card_obj'> <div class=\"demo-card-wide mdl-card mdl-shadow--2dp\"> <div class=\"mdl-card__title\"> <h2 class=\"mdl-card__title-text\">Welcome<\/h2> <\/div> <div class=\"mdl-card__supporting-text\"> <p>Filled in by JS loop<\/p> <\/div> <div class=\"mdl-card__actions mdl-card--border\"> <a class=\"mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect\"> Fit Details <\/a> <\/div> <\/div> <\/div>\"");
+	console.log(x);
+	$(".cardmain").append(x);
+		$(".cardmain").append(x);
+			$(".cardmain").append(x);
+				$(".cardmain").append(x);
 }
