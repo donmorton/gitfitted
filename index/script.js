0c333d1e231a97d859225cdf9520c24d5d6426c7
Donald Morton <d3baseball2@gmail.com>
2017-02-12 09:17:49 -0500

    added

--- index/script.js
+++ index/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$("button").not("#view-source").click(function(){
+	$("button").click(function(){
 		console.log($(this).attr("id"));
 		sort($(this).attr("id"));
 	});
